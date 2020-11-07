c46227bb74e0218ce202f110fa6d8d4171df74d7
Vladimír Zdražil <git@vzdrazil.com>
2020-11-07 16:59:10 +0100

    update lein profiles

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {:dependencies [ [nrepl "0.8.0"]]
+        :plugins [[cider/cider-nrepl "0.25.3"]]
+ :repl {:plugins [[cider/cider-nrepl "0.25.3"]
+                  [lein-cljfmt "0.7.0"]]
+        :dependencies [[nrepl "0.8.0"] ]
+        }}}
+
