00ded8c5160849361ccd0cf862e454e88d9de452
Vladimír Zdražil <git@vzdrazil.com>
2021-01-23 21:34:47 +0100

    change fish prompt

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,23 @@
+function fish_prompt --description 'Write out the prompt'
+    # Save our status
+    set -l last_pipestatus $pipestatus
+
+    set -l color_cwd
+    set -l suffix
+    switch "$USER"
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix '>'
+    end
+
+    echo -n -s "· " (set_color $color_cwd) (prompt_pwd) \
+        (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus) \
+        (set_color normal) " $suffix "
+end
