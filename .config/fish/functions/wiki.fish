7dfcd360d5a27a6f40c946053001f81e09ca600e
Vladimír Zdražil <git@vzdrazil.com>
2021-04-06 09:25:51 +0200

    update wiki function

--- .config/fish/functions/wiki.fish
+++ .config/fish/functions/wiki.fish
@@ -1,4 +1,10 @@
 function wiki --description "Change dir and search wiki"
-     cd "$HOME/vimwiki"
-     vim -c ':copen' -q (rg --vimgrep -S $argv "$HOME/vimwiki" | psub)
+    cd "$HOME/vimwiki"
+    if [ (count $argv) -eq 1 ];
+        vim -c ':copen' -q (rg --vimgrep -S $argv "$HOME/vimwiki" | psub)
+        return
+    else
+        vim -c ':VimwikiIndex'
+        return
+    end
 end
