5f0b892a4c38885b5d8c5f8a3368d0937bfa4604
Vladimír Zdražil <git@vzdrazil.com>
2023-09-04 10:54:21 +0200

    improve yarn workspace completion
    make it smarter

--- .config/fish/completions/yarn.fish
+++ .config/fish/completions/yarn.fish
@@ -1,3 +1,27 @@
 source $__fish_data_dir/completions/yarn.fish
 
-complete -c yarn -n '__fish_seen_subcommand_from workspace' -xa '(if test -f package.json; fd package.json -x 'cat' | jq -r ".name" ;end)'
+function find_workspaces -d "If package.json exists, find all the workspace package"
+    if test -f package.json
+        fd package.json -x cat | jq -r ".name"
+    end
+end
+
+function not_has_suggestion
+    commandline -pc | string match -r 'yarn workspace\s*$'
+end
+
+function not_has_script
+    commandline -pc | string match -r 'yarn workspace\s*\S*\s*$'
+end
+
+function find_scripts -d "Test if a non-switch argument has been given in the current commandline"
+    set -l cmd (commandline -poc)
+
+    # Taken from $__fish_data_dir/completions/yarn.fish, fish version 3.6.1 
+    $cmd run 2>/dev/null | sed '$d' | awk '{ print $3}'
+end
+
+
+complete -c yarn -n '__fish_seen_subcommand_from workspace; and not_has_suggestion' -xa '(find_workspaces)'
+
+complete -c yarn -n '__fish_seen_subcommand_from workspace; and not_has_script' -xa '(find_scripts)'
