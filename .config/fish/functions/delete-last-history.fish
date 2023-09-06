a4b87427905daff80bacea03e39784a325150611
Vladimír Zdražil <git@vzdrazil.com>
2023-09-06 04:20:53 +0200

    update fish shell

--- .config/fish/functions/delete-last-history.fish
+++
@@ -1,5 +0,0 @@
-function delete-last-history --description "Deletes and echo the last command from the shell history."
-    set last_entry (history | head -n 1 | cut -d" " -f2)
-    history delete --exact --case-sensitive -- "$last_entry"
-    echo "$last_entry"
-end
