7032311bfee86f81f8fa65d015ef4d581b0d6d61
Vladimír Zdražil <git@vzdrazil.com>
2023-09-08 12:51:54 +0200

    add perl abbreviations

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,3 +1,12 @@
-abbr --add lg lazygit
 abbr --add g git
+abbr --add lg lazygit
 abbr --add yw 'yarn workspace'
+
+# perl -wnl -e 's/hello/hi/g and print'
+abbr --add pgrep 'perl -wnl -e'
+
+# perl -wnl -e 's/hello/ and print'
+abbr --add psed 'perl -wpl -e'
+
+# use quotemeta
+abbr --add psed-fixed "perl -wpl -e 's/\Qstring\E/replacement/g'"
