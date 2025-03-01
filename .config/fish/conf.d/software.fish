2121d6dc5eb91ad3972c92ab5d27cea368880d65
Vladimír Zdražil <git@vzdrazil.com>
2025-03-01 17:19:50 +0100

    update fish path

--- .config/fish/conf.d/software.fish
+++ .config/fish/conf.d/software.fish
@@ -21,7 +21,7 @@ test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell
 if string match --quiet "$TERM_PROGRAM" vscode
     switch uname
         case Darwin
-            . '/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/contrib/terminal/browser/media/fish_xdg_data/fish/vendor_conf.d/shellIntegration.fish'
+            . 'Contents/Resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration.fish'
         case '*'
             # This is slow
             . (code --locate-shell-integration-path fish)
