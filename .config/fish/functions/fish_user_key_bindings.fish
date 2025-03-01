385aef98c7cb3943568d87f9a8bba208d10804f3
Vladimír Zdražil <git@vzdrazil.com>
2025-03-01 10:50:54 +0100

    update config

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
     bind -k f3 append_pipe_fzf
     fzf_key_bindings
-    bind --erase \cr # restore built-in-fish>=3.6.0 Ctrl+R history
+    # bind --erase \cr # restore built-in-fish>=3.6.0 Ctrl+R history
 end
