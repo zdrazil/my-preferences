0cb74cc54ada43ad01bcbb53ee59d614f28d4fd6
Vladimír Zdražil <git@vzdrazil.com>
2025-03-01 17:28:24 +0100

    remove injection vscode, rely on automatic one
    terminal.integrated.shellIntegration.enabled

--- .config/fish/conf.d/software.fish
+++ .config/fish/conf.d/software.fish
@@ -15,15 +15,3 @@ direnv hook fish | source
 # iTerm 2
 
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
-
-# Visual Studio Code
-
-if string match --quiet "$TERM_PROGRAM" vscode
-    switch uname
-        case Darwin
-            . 'Contents/Resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration.fish'
-        case '*'
-            # This is slow
-            . (code --locate-shell-integration-path fish)
-    end
-end
